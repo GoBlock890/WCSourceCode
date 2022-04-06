cc96166a0db0b3c828c9c30e4987ca24868e3043
GoBlock890 <103117234+GoBlock890@users.noreply.github.com>
2022-04-06 19:14:27 +0700

    Add files via upload

---
+++ src/ReplicatedStorage/Cache/WheatMem.lua
@@ -0,0 +1,36 @@
+local _Data = {}
+local id = 0
+
+local module;module = 
+	{
+		Store = function(Key, Data)
+			_Data[Key] = Data
+
+		end,
+
+		Get = function(id)
+
+			return _Data
+
+		end,
+
+		GetNewIndex = function()
+
+			id += 1
+
+			return math.round(id)
+
+		end,
+
+		Remove = function(Key)
+
+			_Data[Key] = nil
+			id -= 1
+
+		end
+
+
+
+	}
+
+return module
